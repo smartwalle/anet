fe64b74ac9daec00652fefbc4641f44f2d063dfb
SmartWalle <smartwalle@gmail.com>
2018-08-01 15:03:21 +0800

    update

--- transaction_test.go
+++ transaction_test.go
@@ -1,8 +1,10 @@
 package anet4go
 
-import "testing"
+import (
+	"testing"
+)
 
-//func TestAuthorizeNet_CreateTransaction(t *testing.T) {
+func TestAuthorizeNet_CreateTransaction(t *testing.T) {
 //	var p = &CreateTransactionParam{}
 //	p.CreateTransactionRequest.TransactionRequest.TransactionType = K_TRANSACTION_TYPE_CHARGE
 //	p.CreateTransactionRequest.TransactionRequest.Amount = "100"
@@ -30,32 +32,32 @@ import "testing"
 //		t.Fatal(err)
 //	}
 //	fmt.Println(rsp.TransactionResponse.TransId)
-//}
-//
-//func TestAuthorizeNet_Charge(t *testing.T) {
-//	fmt.Println(client.ChargeWithCreditCard("1234", "100", "f", "y", "5145919892544954", "2020-01", "123", "China", "SC", "CD", "123456", "ADD"))
-//}
-//
-//func TestAuthorizeNet_Auth(t *testing.T) {
-//	fmt.Println(client.AuthWithCreditCard("1234", "100", "5145919892544954", "2020-01", "123", "f", "y", "China", "SC", "CD", "123456", "ADD"))
-//}
-//
-//func TestAuthorizeNet_CaptureWithTransId(t *testing.T) {
-//	fmt.Println(client.CaptureWithTransId("60104495440", "100"))
-//}
-//
-//func TestAuthorizeNet_CaptureWithAutoCode(t *testing.T) {
-//	client.CaptureWithAutoCode("MFQFPQ", "100", "5145919892544954", "2020-01", "123")
-//}
-//
-func TestAuthorizeNet_Refund(t *testing.T) {
-	client.RefundWithCreditCard("60106065113", "116.55", "4954", "2020-2")
 }
-//
-//func TestAuthorizeNet_GetTransactionDetails(t *testing.T) {
+
+func TestAuthorizeNet_Charge(t *testing.T) {
+	//fmt.Println(client.ChargeWithCreditCard("12342", "100", "5145919892544954", "2020-01", "123", "f", "y", "China", "SC", "CD", "123456", "123456", "123456", "ADD"))
+}
+
+func TestAuthorizeNet_Auth(t *testing.T) {
+	//fmt.Println(client.AuthWithCreditCard("12343", "100", "5145919892544954", "2020-01", "123", "f", "y", "China", "SC", "CD", "123456", "123456", "123456", "ADD"))
+}
+
+func TestAuthorizeNet_CaptureWithTransId(t *testing.T) {
+	//fmt.Println(client.CaptureWithTransId("60106662170", "300"))
+}
+
+func TestAuthorizeNet_CaptureWithAutoCode(t *testing.T) {
+//	client.CaptureWithAutoCode("MFQFPQ", "100", "5145919892544954", "2020-01", "123")
+}
+
+func TestAuthorizeNet_Refund(t *testing.T) {
+	//fmt.Println(client.RefundWithCreditCard("60106662016", "100", "5145919892544954", "2020-01"))
+}
+
+func TestAuthorizeNet_GetTransactionDetails(t *testing.T) {
 //	fmt.Println(client.GetTransactionDetails("60104455428"))
-//}
-//
-//func TestAuthorizeNet_DebitWithBankAccount(t *testing.T) {
-//	fmt.Println(client.DebitWithBankAccount("1234", "1000", K_ACCOUNT_TYPE_CHECKING, "121042882", "123456789", "John Doe", "", "", "", "", "", "", "", "", "", ""))
-//}
+}
+
+func TestAuthorizeNet_DebitWithBankAccount(t *testing.T) {
+	//fmt.Println(client.DebitWithBankAccount("1234", "1000", K_ACCOUNT_TYPE_CHECKING, "121042882", "123456789", "John Doe", "", "", "", "", "", "", "", "", "", "", "", ""))
+}
